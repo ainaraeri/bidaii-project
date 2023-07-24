280a15912fadbd09c5a67d140a1cdb9a9e5c622e
ainaraeri <ainara@Air-de-Ainara.home>
2023-07-24 09:42:28 +0200

    Updating functionalities

--- src/components/app.js
+++ src/components/app.js
@@ -1,17 +1,21 @@
 import React, { Component } from 'react';
 import moment from "moment";
 
+import PortfolioContainer from './portfolio/porfolio-container';
+
 export default class App extends Component {
   render() {
     return (
       <div className='app'>
         <h1>Ainara Erice Portfolio</h1>
+        <div>{moment().format('MMMM Do YYYY, h:mm:ss a')}</div>
+
         <br></br>
 
-        <h2>Let's create something awesome</h2>
+        <PortfolioContainer />
         <br></br>
-        <div>{moment().format('MMMM Do YYYY, h:mm:ss a')}</div>
       </div>
     );
   }
 }
+
