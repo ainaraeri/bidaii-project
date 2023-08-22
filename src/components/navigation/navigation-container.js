c6f423d3b15a8252abd401aeb7e2fcc04dff9928
ainaraeri <ainara@MacBook-Air-de-Ainara.local>
2023-08-22 21:11:53 +0200

    updating flexbox container

--- src/components/navigation/navigation-container.js
+++ src/components/navigation/navigation-container.js
@@ -8,16 +8,21 @@ export default class NavigationComponent extends Component {
     }
 
     render() {
-        return (
-        <div>
-            <NavLink exact to="/">Home</NavLink>
+      return (
+        <div className="nav-wrapper">
+          <div className="left-side">
+            <NavLink exact to="/">
+              Home
+            </NavLink>
             <NavLink to="/about-me">About</NavLink>
             <NavLink to="/contact">Contact</NavLink>
             <NavLink to="/blog">Blog</NavLink>
-            
-            {true ? <button>Add Blog</button> : null }
+
+            {false ? <button>Add Blog</button> : null}
+          </div>
+
+          <div className="right-side">AINARA ERICE</div>
         </div>
-        );
+      );
     }
-    
 }
--- src/components/portfolio/porfolio-container.js
+++ src/components/portfolio/porfolio-container.js
@@ -4,78 +4,72 @@ import axios from "axios";
 import PortfolioItem from './portfolio-item';
 
 export default class PortfolioContainer extends Component {
-    constructor() {
-        super();
+  constructor() {
+    super();
 
-        this.state = {
-            pageTitle: "Welcome to my portfolio",
-            isLoading: false,
-            data: []
-        };
+    this.state = {
+      pageTitle: "Welcome to my portfolio",
+      isLoading: false,
+      data: [],
+    };
 
-        this.handleFilter = this.handleFilter.bind(this);
+    this.handleFilter = this.handleFilter.bind(this);
+  }
 
-    }
+  handleFilter(filter) {
+    this.setState({
+      data: this.state.data.filter((item) => {
+        return item.category === filter;
+      }),
+    });
+  }
 
-    handleFilter(filter){
+  getPortfolioItems() {
+    axios
+      .get("https://ainaraerice.devcamp.space/portfolio/portfolio_items")
+      .then((response) => {
+        // handle success
         this.setState({
-            data: this.state.data.filter(item => {
-                return item.category === filter;
-            })
-        })
-    }
-
-    getPortfolioItems() {
-        axios
-          .get("https://ainaraerice.devcamp.space/portfolio/portfolio_items")
-          .then (response => {
-            // handle success
-            this.setState({
-                data: response.data.portfolio_items
-            })
-          })
-          .catch (error => {
-            // handle error
-            console.log(error);
-          });
-      }
-
-    portfolioItems() {
-        return this.state.data.map((item) => {
-          //console.log("portfolio item", item);
-          debugger;
-          return (
-            <PortfolioItem
-              key={item.id}
-              title={item.name}
-              url={item.url}
-              slug={item.id}
-            />
-          );
+          data: response.data.portfolio_items,
         });
+      })
+      .catch((error) => {
+        // handle error
+        console.log(error);
+      });
+  }
+
+  portfolioItems() {
+    return this.state.data.map((item) => {
+      return <PortfolioItem key={item.id} item={item} />;
+    });
+  }
+
+  componentDidMount() {
+    this.getPortfolioItems();
+  }
+
+  render() {
+    if (this.state.isLoading) {
+      return <div>Loading...</div>;
     }
 
-    componentDidMount() {
-        this.getPortfolioItems();
-    }
+    return (
+      <div>
+        <h2>{this.state.pageTitle} </h2>
 
-    render () {
-        if (this.state.isLoading) {
-            return <div>Loading...</div>;
-        }
+        <button onClick={() => this.handleFilter("eCommerce")}>
+          eCommerce
+        </button>
+        <button onClick={() => this.handleFilter("Scheduling")}>
+          Scheduling
+        </button>
+        <button onClick={() => this.handleFilter("Enterprise")}>
+          Enterprise
+        </button>
 
-        return (
-            <div>
-                <h2>{this.state.pageTitle} </h2>
-                
-                <button onClick={() => this.handleFilter('eCommerce')}>eCommerce</button>
-                <button onClick={() => this.handleFilter('Scheduling')}>Scheduling</button>
-                <button onClick={() => this.handleFilter('Enterprise')}>Enterprise</button>
-
-
-                {this.portfolioItems()}
-                
-            </div>
-        );
-    }
+        {this.portfolioItems()}
+      </div>
+    );
+  }
 } 
