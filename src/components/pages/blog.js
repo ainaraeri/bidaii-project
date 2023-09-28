e78b1af2f810abb02963978f0cca4ef122708bdb
ainaraeri <ainara@MacBook-Air-de-Ainara.local>
2023-09-28 17:12:36 +0200

    modals creating and updating

--- src/components/pages/blog.js
+++ src/components/pages/blog.js
@@ -3,6 +3,7 @@ import { Link } from 'react-router-dom/cjs/react-router-dom.min';
 import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
 import axios from "axios";
 import BlogItem from '../blogs/blog-item';
+import BlogModal from "../modals/blog-modal";
 
 class Blog extends Component {
   constructor() {
@@ -13,11 +14,26 @@ class Blog extends Component {
       totalCount: 0,
       currentPage: 0,
       isLoading: true,
+      blogModalIsOpen: false 
     };
 
     this.getBlogItems = this.getBlogItems.bind(this);
     this.onScroll = this.onScroll.bind(this);
     window.addEventListener("scroll", this.onScroll, false);
+    this.handleNewBlogClick = this.handleNewBlogClick.bind(this);
+    this.handleModalClose = this.handleModalClose.bind(this);
+  }
+
+  handleModalClose() {
+    this.setState ({
+      blogModalIsOpen: false
+    })
+  }
+
+  handleNewBlogClick() {
+    this.setState({
+      blogModalIsOpen:true
+    })
   }
 
   onScroll() {
@@ -70,8 +86,18 @@ class Blog extends Component {
     const blogRecords = this.state.blogItems.map((blogItem) => {
       return <BlogItem key={blogItem.id} blogItem={blogItem} />;
     });
+
+
     return (
       <div className="blog-container">
+
+        <BlogModal 
+        handleModalClose = {this.handleModalClose}
+        modalIsOpen = {this.state.blogModalIsOpen}/>
+        <div className='new-blog-link'>
+          <a onClick={this.handleNewBlogClick}>Open Modal!</a>
+        </div>
+
         <div className="content-container">{blogRecords}</div>
 
         {this.state.isLoading ? (
