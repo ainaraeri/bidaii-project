a945f5b39b99fabf3225f93a9a66da64128164c6
ainaraeri <ainara@MacBook-Air-de-Ainara.local>
2023-09-29 12:39:33 +0200

    middle day 29/09/2023

--- src/components/modals/blog-modal.js
+++ src/components/modals/blog-modal.js
@@ -1,17 +1,45 @@
 import React, { Component } from 'react';
 import ReactModal from "react-modal";
 
+import BlogForm from '../blogs/blog-form';
+
+ReactModal.setAppElement(".app-wrapper");
+
 export default class BlogModal extends Component {
     constructor(props) {
         super(props);
+
+        this.customStyles = {
+          content: {
+            top: "50%",
+            left: "50%",
+            right: "auto",
+            marginRight: "-50%",
+            transform: "translate(-50%, -50%)",
+            width: "600px",
+          },
+          overlay: {
+            backgroundColor: "rgba(1, 1, 1, 0.75)",
+          },
+        };
+
+        this.handleSuccessfulFormSubmission = this.handleSuccessfulFormSubmission.bind(this);
+    }
+
+    handleSuccessfulFormSubmission(blog) {
+        console.log("blog form blog form", blog);
     }
 
   render() {
     return (
-      <ReactModal onRequestClose={() => {
-        this.props.handleModalClose();
-      }} isOpen={this.props.modalIsOpen}>
-        <h1>Using React Modal</h1>
+      <ReactModal
+        style={this.customStyles}
+        onRequestClose={() => {
+          this.props.handleModalClose();
+        }}
+        isOpen={this.props.modalIsOpen}
+      >
+        <BlogForm handleSuccessfulFormSubmission={this.handleSuccessfulFormSubmission} />
       </ReactModal>
     );
   }
