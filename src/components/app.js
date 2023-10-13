312cd6dbe033ab44c25b8905d4ec6d1c1b90c05c
ainaraeri <ainara@MacBook-Air-de-Ainara.local>
2023-10-13 14:29:20 +0200

    navbar updated

--- src/components/app.js
+++ src/components/app.js
@@ -2,11 +2,14 @@ import React, { Component } from 'react';
 import NavigationContainer from './navigation/navigation-container';
 
 export default class App extends Component {
+
   render() {
     return (
       <div>
         <NavigationContainer/>
       </div>
+
+      
     );
   }
 }
