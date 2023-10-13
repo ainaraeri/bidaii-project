c79b32ea5685de441726df906d15c28e357b37b9
ainaraeri <ainara@MacBook-Air-de-Ainara.local>
2023-10-13 10:17:54 +0200

    initial commit

--- src/components/navigation/navigation-container.js
+++ src/components/navigation/navigation-container.js
@@ -1,78 +1,18 @@
-import React from 'react';
-import { NavLink } from 'react-router-dom';
-import axios from "axios";
-import { withRouter } from "react-router";
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import React, { Component } from 'react';
 
-
-const NavigationComponent  = (props) => {
-  const dynamicLink = (route, linkText) => {
-  return (
-    <div className="nav-link-wrapper">
-      <NavLink to={route} activeClassName="nav-link-active">
-        {linkText}
-      </NavLink>
-    </div>
-  );
-};
-
-const handleSignOut = () => {
-  axios
-    .delete("https://api.devcamp.space/logout", { withCredentials: true })
-    .then((response) => {
-      if (response.status === 200) {
-        props.history.push("/");
-        props.handleSuccessfulLogout();
-      }
-      return response.data;
-    })
-    .catch((error) => {
-      console.log("Error signing out", error);
-    });
-};
-
-
-  return (
-    <div className="nav-wrapper">
-      <div className="left-side">
-        <div className="nav-link-wrapper">
-          <NavLink exact to="/" activeClassName="nav-link-active">
-            Home
-          </NavLink>
-        </div>
-        <div className="nav-link-wrapper">
-          <NavLink to="/about-me" activeClassName="nav-link-active">
-            About
-          </NavLink>
-        </div>
-        <div className="nav-link-wrapper">
-          <NavLink to="/blog" activeClassName="nav-link-active">
-            Blog
-          </NavLink>
+export default class NavigationContainer extends Component {
+  render() {
+    return (
+      <div className='nav-wrapper'>
+        <div className='left-side'>
+          <a>INICIO</a>
         </div>
 
-        <div className="nav-link-wrapper">
-          <NavLink to="/contact" activeClassName="nav-link-active">
-            Contact
-          </NavLink>
+        <div className='right-side'>
+          <button>INICIA SESIÓN</button>
         </div>
-
-        {props.loggedInStatus === "LOGGED_IN"
-          ? dynamicLink("/portfolio-manager", "Portfolio Manager")
-          : null}
+        
       </div>
-
-      <div className="right-side">
-        AINARA ERICE
-        {props.loggedInStatus === "LOGGED_IN" ? (
-          <a onClick={handleSignOut}>
-          <FontAwesomeIcon icon="sign-out-alt" />
-            </a>
-        ) : null}
-      </div>
-    </div>
-  );
-};
-
-    
-export default withRouter(NavigationComponent);
+    );
+  }
+}
