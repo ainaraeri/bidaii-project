887c3c904b0ee609360b4423703b8b524e31b66c
ainaraeri <ainara@MacBook-Air-de-Ainara.local>
2023-10-18 15:53:11 +0200

    changing server.js

--- server.js
+++ server.js
@@ -1,50 +1,25 @@
-const express = require('express');
-const mongoose = require('mongoose');
-const { run } = require('../mongo-connect'); 
-
-
-const app = express();
-const port = process.env.PORT || 3001;
+const { MongoClient, ServerApiVersion } = require('mongodb');
 const uri = process.env.MONGODB_URI;
 
-// Conecta a MongoDB
-mongoose.connect('mongodb://localhost:27017/tu-base-de-datos', {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  }
 });
 
-const db = mongoose.connection;
-
-db.on('error', console.error.bind(console, 'Error de conexión a MongoDB:'));
-db.once('open', () => {
-  console.log('Conexión exitosa a MongoDB');
-  
-  // Llama a la función run para conectar a MongoDB Atlas u otra base de datos
-  run().then(() => {
-    console.log('Conexión exitosa a la base de datos externa');
-  }).catch(error => {
-    console.error('Error al conectar a MongoDB:', error);
-  });
-});
-
-async function run() {
+/*/async function run() {
   try {
-    console.log('Intentando conectar a MongoDB...');
-    // Resto del código ...
-  } catch (error) {
-    console.error('Error al conectar a MongoDB:', error);
-    throw error; // Para propagar el error y manejarlo en la llamada a run
+    // Connect the client to the server	(optional starting in v4.7)
+    await client.connect();
+    // Send a ping to confirm a successful connection
+    await client.db("admin").command({ ping: 1 });
+    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+  } finally {
+    // Ensures that the client will close when you finish/error
+    await client.close();
   }
 }
-
-// Sirve los archivos estáticos desde la carpeta 'dist'
-app.use(express.static(__dirname + '/dist/'));
-
-// Ruta para todas las URLs; sirve el index.html
-app.get(/.*/, (req, res) => {
-  res.sendFile(__dirname + '/dist/index.html');
-});
-
-app.listen(port, () => {
-  console.log(`Servidor escuchando en el puerto ${port}`);
-});
+run().catch(console.dir);/*/
