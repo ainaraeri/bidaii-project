e5825aff6cd2965e8765fbd48e9d7328d44bc6ba
ainaraeri <ainara@MacBook-Air-de-Ainara.local>
2023-10-18 15:36:02 +0200

    updating server.js

--- server.js
+++ server.js
@@ -5,6 +5,7 @@ const { run } = require('../mongo-connect');
 
 const app = express();
 const port = process.env.PORT || 3001;
+const uri = process.env.MONGODB_URI;
 
 // Conecta a MongoDB
 mongoose.connect('mongodb://localhost:27017/tu-base-de-datos', {
