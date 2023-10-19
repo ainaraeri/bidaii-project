dab2843b20a47d1918ad852365fdfc2bfa758a81
ainaraeri <ainara@MacBook-Air-de-Ainara.local>
2023-10-19 09:28:44 +0200

    fixed mongodb connection

--- server.js
+++ server.js
@@ -1,24 +1,29 @@
 const { MongoClient, ServerApiVersion } = require('mongodb');
 const express = require('express');
 const mongoose = require('mongoose');
-const uri = process.env.MONGODB_URI||"ainaraerice:<password>@cluster0.djlqrpw.mongodb.net/?retryWrites=true&w=majority";
+const uri = "mongodb+srv://ainaraerice:mioasisdepaz@cluster0.djlqrpw.mongodb.net/?retryWrites=true&w=majority";
 
 const port = process.env.PORT || 8080;
 
-
-mongoose.connect(uri, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  }
 });
 
-const db = mongoose.connection;
-
-// Maneja los eventos de conexión y error
-db.on('error', console.error.bind(console, 'Error de conexión a MongoDB:'));
-db.once('open', () => {
-  console.log('Conexión exitosa a MongoDB');
-  // Aquí puedes inicializar y configurar tu aplicación Express
-  app.listen(3001, () => {
-    console.log('Servidor escuchando en el puerto 3001');
-  });
-});
+async function run() {
+  try {
+    // Connect the client to the server	(optional starting in v4.7)
+    await client.connect();
+    // Send a ping to confirm a successful connection
+    await client.db("admin").command({ ping: 1 });
+    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+  } finally {
+    // Ensures that the client will close when you finish/error
+    await client.close();
+  }
+}
+run().catch(console.dir);
