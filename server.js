6eb68b5d4412b1154179dc9512866481c8e171af
ainaraeri <ainara@MacBook-Air-de-Ainara.local>
2023-10-19 12:16:38 +0200

    changed express get

--- server.js
+++ server.js
@@ -7,8 +7,8 @@ const app = express(); // Define 'app' como una instancia de Express
 
 app.use(express.static('static'));
 
-app.get('/*', (req, res) => {
-  res.sendFile(path.join(__dirname, '../static/index.html'));
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, 'static', 'index.html'));
 });
 
 const port = process.env.PORT || 8080;
