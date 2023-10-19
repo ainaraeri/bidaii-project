a267c567d2f45b2915fd543d33d2883b8dedc939
ainaraeri <ainara@MacBook-Air-de-Ainara.local>
2023-10-19 13:41:02 +0200

    changing webpack.config.js

--- README.md
+++
@@ -1,1 +0,0 @@
-# Ainara Erice React Portfolio Application
--- src/reducers/index.js
+++
@@ -1,8 +0,0 @@
-import { combineReducers } from 'redux';
-import App from '../components/app';
-
-const rootReducer = combineReducers({
-  state: (state = {}) => state
-});
-
-export default rootReducer;
