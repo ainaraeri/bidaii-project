4e35879541a1b9c5dc3e7bc87fd72dcf89df6c98
ainaraeri <ainara@MacBook-Air-de-Ainara.local>
2023-10-20 14:00:29 +0200

    script in index.html

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,5 +1,14 @@
 import { combineReducers } from 'redux';
 import App from '../components/app';
+import React from "react";
+import ReactDOM from "react-dom/client";
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
 
 const rootReducer = combineReducers({
   state: (state = {}) => state
