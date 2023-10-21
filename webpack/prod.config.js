262ace54b681955633fc2b799016f0a8c4801980
ainaraeri <ainara@MacBook-Air-de-Ainara.local>
2023-10-21 17:31:17 +0200

    las img?

--- webpack/prod.config.js
+++ webpack/prod.config.js
@@ -72,6 +72,16 @@ module.exports = merge(webpackCommon, {
           },
         ],
       },
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env'],
+          },
+        },
+      },
     ],
   },
   
