f783bafcdfa24afaeb17ff0c0a27178846eea938
ainaraeri <ainara@MacBook-Air-de-Ainara.local>
2023-10-22 12:59:25 +0200

    reset a conseguido que al registrarse me lleve a user-dashboard

--- static/assets/README.md
+++
@@ -1,2 +0,0 @@
-# Ainara Erice React Portfolio Application
-
--- webpack/postcss.config.js
+++
@@ -1,9 +0,0 @@
-const MiniCssExtractPlugin = require('mini-css-extract-plugin');
-
-module.exports = {
-  plugins: {
-    autoprefixer: {},
-    // Otros plugins de PostCSS según tus necesidades
-  }
-};
-
